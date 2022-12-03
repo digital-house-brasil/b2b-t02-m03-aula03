6b433441459b2caa05a709472368a0bcb1508b8a
joao candido <joan.jb56@gmail.com>
2022-12-03 15:58:50 -0300

    exercicio1.5

---
+++ Exercicio1.5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+    //Faça um programa que receba o nome e depois o sobrenome, no final deve exibir
+    // o resultado nome + sobrenome
+    //Utilize o operador + para concatenar 2 variáveis do tipo string
+
+    string nome = "", sobrenome = "";
+    Console.Write("Olá poderia responder as perguntas? \n");
+    Console.Write("digite o seu nome: ");
+    nome = Console.ReadLine();
+    Console.Write("digite o seu sobrenome: ");
+    sobrenome = Console.ReadLine();
+    Console.WriteLine("obrigado por responder "+nome + sobrenome);
+    Console.ReadKey();
+
+
+
+
+
+       
+    }
+}
