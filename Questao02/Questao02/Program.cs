129a58ff67e0ad1bb72dcd9a6b33a04cfc48a1ca
Lucas Ferreira <Lucasfdasilva2002@gmail.com>
2022-12-05 10:07:07 -0400

    Adicionando os exercícios

---
+++ Questao02/Questao02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            float valor = float.Parse(Console.ReadLine());
+            if (valor >= 0)
+            {
+                Console.WriteLine("O valor é positivo");
+            }
+            else
+            {
+                Console.WriteLine("O valor é negativo");
+            }
+        }
+    }
+}
---
+++ Questao06/Questao06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+            if (idade>= 18)
+            {
+                Console.WriteLine("Maior de idade");
+            }
+            else
+            {
+                Console.WriteLine("Menor de idade");
+            }
+        }
+    }
+}
