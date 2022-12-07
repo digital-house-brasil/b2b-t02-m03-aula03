075b0449926e4e6e22d669e24610f0aacdbf8e54
joao candido <joan.jb56@gmail.com>
2022-12-07 14:46:17 -0300

    criando exercicio teste

---
+++ Exercicio1.7.cs
@@ -0,0 +1,18 @@
+using System;
+
+class primeiro{
+
+    //variavel global
+     static int n = 1;
+
+    //metodo principal do programa
+    static void Main(){
+
+    Console.WriteLine("o valor de n = " +n);
+    }
+
+    //criando metodo teste
+    void teste(){
+
+    }
+}
